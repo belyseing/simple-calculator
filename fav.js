db9a649b44b9cbbdfe8557d2c4d528d606069743
belyseing <162991256+belyseing@users.noreply.github.com>
2025-03-24 11:19:36 +0200

    simple calculator

---
+++ fav.js
@@ -0,0 +1,26 @@
+// const, let, var
+
+const favoritefood = "rice";
+const favoritecolor = "red";
+const hobby = "swimming";
+
+console.log(
+  "My favorite food is:" + favoritefood,
+  "My favorite color is:" + favoritecolor,
+  "My hobby is:" + hobby
+);
+
+// declare num1 as variable
+
+let num1 = 3;
+let num2 = 5;
+
+sum = num1 + num2;
+differences = num1 - num2;
+quotient = num1 / num2;
+
+console.log(
+  "The sum is:" + sum,
+  "The differences is:" + differences,
+  "The quotient is:" + quotient
+);
